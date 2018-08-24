32aebe2a70476c1ae626bc3c5d51f84664e3fe4a
etudiant <etudiant@etudiant-6.local>
2018-08-24 11:28:54 +0200

    ChansonCell

--- Lecteur video/TableauController.swift
+++ Lecteur video/TableauController.swift
@@ -16,6 +16,8 @@ class TableauController: UIViewController, UITableViewDelegate, UITableViewDataS
     
     var chansons = [Chanson]()
     
+    let identifiantCell = "ChansonCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,7 +32,23 @@ class TableauController: UIViewController, UITableViewDelegate, UITableViewDataS
 
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let chanson = chansons[indexPath.row]
+        
+      /*  let cell = UITableViewCell()
+        cell.textLabel?.text = chanson.titre
+        
+        return cell*/
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: identifiantCell) as? ChansonCell{
+            cell.creerCell(chanson)
+            return cell
+        }
+        
         return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 170
     }
     
     func ajouterChanson() {
