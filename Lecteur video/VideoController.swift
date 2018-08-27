58b56a3d25da71c4748fbed217b58986335b204e
etudiant <etudiant@etudiant-6.local>
2018-08-27 11:40:23 +0200

    Commit de fin

--- Lecteur video/VideoController.swift
+++ Lecteur video/VideoController.swift
@@ -7,21 +7,37 @@
 //
 
 import UIKit
+import WebKit
 
 class VideoController: UIViewController {
 
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    
+    
+    
+    
+    
     var chanson: Chanson?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        //view.backgroundColor = .blue
         if chanson != nil{
-            title = chanson!.titre
+            //title = chanson!.titre
+            chargerVideo(chanson: chanson!)
         }
         
     }
 
+    func chargerVideo(chanson: Chanson){
+        if let url = URL(string: chanson.videoUrl){
+        let requete = URLRequest(url: url)
+            webView.load(requete)
+        }
+    }
  
 }
